c74157e6700d05842711eae196b7cf573a9548f6
Blackbeltsaru <beldinryan@gmail.com>
2019-08-23 20:45:00 -0500

    corvus globals

--- corvusExamples/corvus-engine/src/index.js
+++ corvusExamples/corvus-engine/src/index.js
@@ -3,3 +3,9 @@ import EntryPoint from './Corvus/EntryPoint.js';
 import CorvusKey from './Corvus/Core/KeyCodes.js';
 import CorvusMouseButton from './Corvus/Core/MosueButtonCodes.js';
 import WindowAnimator from './test/renderer/WindowContext.js';
+
+
+const CApplication = Application;
+const Corvus = EntryPoint;
+const CKey = CorvusKey;
+const CMouseButton = CorvusMouseButton;
